b112f51483bb4b8d8ff160b8b735bb947e9805d3
iohn2000 <johannes.fleck@gmail.com>
2016-05-08 20:56:38 +0200

    Update StockExchangeLosses.cs

--- StockExchangeLosses.cs
+++ StockExchangeLosses.cs
@@ -1,1 +1,28 @@
-//stock exchange
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] inputs = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++)
+        {
+            int v = int.Parse(inputs[i]);
+        }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("answer");
+    }
+}
