59ffb2009c5128c477e19782164c2be3d64e3c93
Johannes Fleck <johannes.fleck@gmail.com>
2016-05-08 21:52:17 +0200

    first version, too slow ...

--- StockExchangeLosses.cs
+++ StockExchangeLosses.cs
@@ -11,18 +11,48 @@ using System.Collections.Generic;
  **/
 class Solution
 {
-    static void Main(string[] args)
-    {
-        int n = int.Parse(Console.ReadLine());
-        string[] inputs = Console.ReadLine().Split(' ');
-        for (int i = 0; i < n; i++)
-        {
-            int v = int.Parse(inputs[i]);
-        }
+	static void Main(string[] args)
+	{
+		List<int> stocks = new List<int>();
 
-        // Write an action using Console.WriteLine()
-        // To debug: Console.Error.WriteLine("Debug messages...");
+		int n = int.Parse(Console.ReadLine());
+		string[] inputs = Console.ReadLine().Split(' ');
+		for (int i = 0; i < n; i++)
+		{
+			int v = int.Parse(inputs[i]);
+			Console.Error.Write (inputs[i] + ", ");
+			stocks.Add(v);
+		}
 
-        Console.WriteLine("answer");
-    }
+
+		List<int> sortedMax = new List<int>(stocks);
+		sortedMax.Sort((x,y)=> y.CompareTo(x));
+
+		Console.Error.Write("\r\nSorted List :");
+		foreach (int i in sortedMax) Console.Error.Write(i.ToString()+ ", ");
+		Console.Error.WriteLine("");
+
+		int min, maxLoss = 0, indexMin, indexMax;
+
+		Console.Error.Write("\r\nReverse Max : ");
+		for (int reversIndx = sortedMax.Count - 1; reversIndx > -1; reversIndx--) 
+		{
+			Console.Error.Write (sortedMax[reversIndx].ToString() + ", ");
+			min = sortedMax [reversIndx];
+			indexMin = stocks.IndexOf(min);
+			foreach (int maxValue in sortedMax)
+			{
+				indexMax = stocks.IndexOf(maxValue);
+				if (indexMax < indexMin)
+				{
+					maxLoss = Math.Min(maxLoss, (maxValue - min) * -1);
+					break;
+				}
+			}
+		}
+
+
+		Console.Error.WriteLine ("\r\n");
+		Console.WriteLine(maxLoss.ToString());
+	}
 }
